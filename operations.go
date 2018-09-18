9d97cfac5d4fb1a2d76fb511dd9bf472d026cb5c
Michael Teuscher <michael.teuscher@pm.me>
2018-09-18 13:31:22 +0200

    Renamed objectID to resource

--- operations.go
+++ operations.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// Delete removes a single object from a collection
-func (fs *FlatStorage) Delete(collection string, objectID string) error {
+// Delete removes a single resource from a collection
+func (fs *FlatStorage) Delete(collection string, resource string) error {
 	return fmt.Errorf("Not implemented")
 }
 
@@ -14,9 +14,9 @@ func (fs *FlatStorage) DeleteAll(collection string) error {
 	return fmt.Errorf("Not implemented")
 }
 
-// Exists checks if an object is present in a collection
-func (fs *FlatStorage) Exists(collection string, objectID string) bool {
-	return false
+// Exists checks if a resource is present in a collection
+func (fs *FlatStorage) Exists(collection string, resource string) bool {
+	return fs.resourceExists(collection, resource)
 }
 
 // CollectionExists checks if a collection exists
@@ -24,17 +24,17 @@ func (fs *FlatStorage) CollectionExists(collection string) bool {
 	return false
 }
 
-// Read reads a single object from a collection into an interface instance
-func (fs *FlatStorage) Read(collection string, objectID string, out interface{}) error {
+// Read reads a single resource from a collection into an interface instance
+func (fs *FlatStorage) Read(collection string, resource string, out interface{}) error {
 	return fmt.Errorf("Not implemented")
 }
 
-// ReadAll reads all objects from a collection into an interface array of objectType
-func (fs *FlatStorage) ReadAll(collection string, objectID string, objectType interface{}) ([]interface{}, error) {
+// ReadAll reads all resources from a collection into an interface array of resourceType
+func (fs *FlatStorage) ReadAll(collection string, resource string, resourceType interface{}) ([]interface{}, error) {
 	return nil, fmt.Errorf("Not implemented")
 }
 
 // Write writes a single object into a collection from an interface instance
-func (fs *FlatStorage) Write(collection string, objectID string, object interface{}) error {
+func (fs *FlatStorage) Write(collection string, resource string, object interface{}) error {
 	return fmt.Errorf("Not implemented")
 }
