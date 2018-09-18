ec6e32282e8a0b43bc057aad3f65936f04432795
Michael Teuscher <michael.teuscher@pm.me>
2018-09-19 00:23:13 +0200

    Adding DeleteAll tests

--- operations_test.go
+++ operations_test.go
@@ -55,20 +55,6 @@ func TestFlatStorage_Delete(t *testing.T) {
 			},
 			wantErr: false,
 		},
-		{
-			name: "Verify deletion",
-			fields: fields{
-				logger:  logrus.New(),
-				mutex:   &sync.Mutex{},
-				mutexes: make(map[string]*sync.Mutex, 0),
-				path:    "/tmp/flatstorage",
-			},
-			args: args{
-				collection: "test",
-				resource:   "deleteme",
-			},
-			wantErr: pathExists("/tmp/flatstorage/test/deleteme"),
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -80,6 +66,65 @@ func TestFlatStorage_Delete(t *testing.T) {
 			}
 			if err := fs.Delete(tt.args.collection, tt.args.resource); (err != nil) != tt.wantErr {
 				t.Errorf("FlatStorage.Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFlatStorage_DeleteAll(t *testing.T) {
+	os.MkdirAll("/tmp/flatstorage/test2", 0777)
+	type fields struct {
+		mutex   *sync.Mutex
+		mutexes map[string]*sync.Mutex
+		path    string
+		logger  *logrus.Logger
+	}
+	type args struct {
+		collection string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Test deletion of inexistent collection",
+			fields: fields{
+				logger:  logrus.New(),
+				mutex:   &sync.Mutex{},
+				mutexes: make(map[string]*sync.Mutex, 0),
+				path:    "/tmp/flatstorage",
+			},
+			args: args{
+				collection: "test3",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Delete existing collection",
+			fields: fields{
+				logger:  logrus.New(),
+				mutex:   &sync.Mutex{},
+				mutexes: make(map[string]*sync.Mutex, 0),
+				path:    "/tmp/flatstorage",
+			},
+			args: args{
+				collection: "test2",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := &FlatStorage{
+				mutex:   tt.fields.mutex,
+				mutexes: tt.fields.mutexes,
+				path:    tt.fields.path,
+				logger:  tt.fields.logger,
+			}
+			if err := fs.DeleteAll(tt.args.collection); (err != nil) != tt.wantErr {
+				t.Errorf("FlatStorage.DeleteAll() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
